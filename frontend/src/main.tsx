5b411ba79f4ab71d2017463b8ce35d0e38737397
Zach Molzner <ZAMolzner@gmail.com>
2026-03-16 13:58:32 -0700

    Fix import quotes and formatting in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { DashboardPage } from "./pages/DashboardPage";
 
-import { DashboardPage } from './pages/DashboardPage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DashboardPage />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
